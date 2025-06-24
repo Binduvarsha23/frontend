38511ab77c830cd1ef41c6c689ad5cbbe5de3bec
Binduvarsha23 <123391226+Binduvarsha23@users.noreply.github.com>
2025-06-24 20:58:18 +0530

    Add files via upload

---
+++ src/components/UnderConstruction.js
@@ -0,0 +1,12 @@
+// components/UnderConstruction.js
+import React from "react";
+import { Container } from "react-bootstrap";
+
+const UnderConstruction = () => (
+  <Container className="text-center mt-5">
+    <h2>🚧 This section is under construction</h2>
+    <p>We’re working hard to bring it to you soon!</p>
+  </Container>
+);
+
+export default UnderConstruction;
